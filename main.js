f1fd7a45e100c48315862ea6a9a5a5acd6b03bf1
iCaioReis <caioreis3186@gmail.com>
2023-09-09 21:29:28 -0300

    Add try and reset functions

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
 function handleClick(event) {
+    console.log(xAttempts)
     event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber")
-    console.log(inputNumber.value)
+
+    if(inputNumber.value == randomNumber){
+        document.querySelector(".screen1").classList.add("hide")
+        document.querySelector(".screen2").classList.remove("hide")
+
+        document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
+    }
+
+    inputNumber.value = ""
+    xAttempts++;
 }
+
+function tryAgain(event){
+    
+    document.querySelector(".screen1").classList.remove("hide")
+    document.querySelector(".screen2").classList.add("hide")
+    xAttempts = 1
+}
+
+//Eventos
+
+const btnTry = document.querySelector('#btnTry')
+btnTry.addEventListener('click', handleClick)
+
+const btnReset = document.querySelector('#btnReset')
+btnReset.addEventListener('click', tryAgain)
